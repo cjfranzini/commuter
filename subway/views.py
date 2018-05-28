81e078be9f819e814721ba145f891d7544a502e8
cjfranzini <cjfranzini@gmail.com>
2018-05-28 19:50:03 -0400

    basic index view

--- subway/views.py
+++ subway/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from subway.models import *
 
 # Create your views here.
+def index(request):
+    trip_list = Trip.objects.order_by('-date')[:5]
+    context = {'trip_list': trip_list}
+    return render(request, 'subway/index.html', context)
+    
+    return render(request, 'polls/index.html', context)
